546d07298b5c0d3b77e41fd5ab1ba65bc18b71c7
gimhanis1999 <146055488+gimhanis1999@users.noreply.github.com>
2023-10-15 16:30:16 +0800

    create reusable component for nav

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,62 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<!--divinectorweb.com-->
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CareLink</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('assets/css/custom.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                <img class="logo" src="/img/logo.jpg" alt="">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-                <ul class="navbar-nav mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#about">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#services">Services</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#portfolio">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#team">Team</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#contact">Contact</a>
-                    </li>
-                    <!-- Registration and Login buttons aligned with other nav items -->
-                    <li class="nav-item">
-                        <a href="/register" class="nav-link"><button class="btn btn-success reg">Registration</button></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/login" class="nav-link"><button class="btn btn-success reg1">Login</button></a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
 
-    <!-- ... (the rest of your content) ... -->
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-</body>
-</html>
+     @extends('include.index')
 
 
       <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
@@ -384,8 +327,7 @@
         </div>
     </footer>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-      </body>
-      </html>
+   
 
 
 
