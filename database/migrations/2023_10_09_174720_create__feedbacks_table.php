304b0dfbc9e9f23069a9e7938188f3cd91fe29ca
gimhanis1999 <146055488+gimhanis1999@users.noreply.github.com>
2023-10-15 18:42:00 +0800

    database migration files
    login table was renamed as users

---
+++ database/migrations/2023_10_09_174720_create__feedbacks_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->unsignedBigInteger('User_id');
+            $table->foreign('User_id')->references('User_id')->on('user');
+            $table->text('Comments');
+            $table->string('Rates');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+};
